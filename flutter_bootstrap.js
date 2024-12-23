e88f9bea4bc188909c99f4d77ad55145573a5c1f
anand-106 <136190059+anand-106@users.noreply.github.com>
2024-12-23 21:14:02 +0530

    LIGHT MODE

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"b8800d88be4866db1b15f8b954ab2573bba996
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "295181397"
+    serviceWorkerVersion: "2921312291"
   }
 });
